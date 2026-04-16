ed4c04e9b1fbef4d058b3c27c79f6b7cd5c5197e
adansaprudinxyz <31801279-adansaprudinxyz@users.noreply.replit.com>
2026-04-16 16:04:34 +0000

    Git commit prior to merge

--- test/index.js
+++ test/index.js
@@ -1,24 +1,25 @@
 const property = require('../src/database/models/property.js');
 
-for (let i = 1; i <= 2; i++) {
-  function getAddress(code) {
-    const itemsPerBlock = 100;
-    const block = String.fromCharCode(65 + Math.floor((code - 1) / itemsPerBlock));
-    const num = ((code - 1) % itemsPerBlock) + 1;
-    return `${block}-${num}`;
-  }
-
-  const address = getAddress(i);
-  const price = Math.floor(Math.random() * (1000000 - 100000 + 1)) + 100000;
-  const description = `This is a property located at ${address}.`;
-
-  const newProperty = new property({
-    id: i,
-    name: address,
-    price: price,
-    description: description
-  });
-  newProperty.save()
-    .then(() => console.log(`Property ${address} saved successfully`))
-    .catch(err => console.error(`Error saving property ${address}:`, err))
+function getAddress(code) {
+  const itemsPerBlock = 100;
+  const block = String.fromCharCode(65 + Math.floor((code - 1) / itemsPerBlock));
+  const num = ((code - 1) % itemsPerBlock) + 1;
+  return `${block}-${num}`;
 }
+
+function generateProperties() {
+   const properties = [];
+  for (let i = 1; i <= 1000; i++) {
+    const address = getAddress(i);
+    properties.push({
+      id: i,
+      name: `Property ${i}`,
+      price: Math.floor(Math.random() * 1000000),
+      description: `This is property ${i} located at ${address}.`
+    });
+  } return properties;
+}
+const properties = generateProperties();
+property.insertMany(properties).then(() => {
+  console.log('Properties inserted successfully');
+});
